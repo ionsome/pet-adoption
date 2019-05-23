42f2fb536633dff662d1dcf8d2b111aec3af2430
Maxim Adrian <maksim.adrian76@gmail.com>
2019-05-23 12:55:34 +0300

    Убрал надпись My pets, когда у пользователя нет питомцев

--- client/src/add/Page.js
+++ client/src/add/Page.js
@@ -55,7 +55,7 @@ class CardsPage extends Component {
         let pageContent;
         let myCards;
 
-        if (this.state.Cards)
+        if (this.state.Cards.length > 0)
         {
             myCards = (
             <div>
